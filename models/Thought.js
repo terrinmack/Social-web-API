aa59983d2c2ad3da2a048444979e9dcd9eee6303
Your Name <terrimack15@gmail.com>
2023-05-07 18:12:47 -0700

    All requests seem to be working!

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,68 @@
 const { Schema, model } = require('mongoose');
-const User = require('./User');
-const reactionSchema = require('./Reaction');
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timeStamp) => new Date(timeStamp).toDateString(),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
 
+const thoughtSchema = new Schema(
+    { 
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (timeStamp) => new Date(timeStamp).toDateString(),
+        },
+        username: { 
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
 
